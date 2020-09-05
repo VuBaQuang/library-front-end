949588aa635f46c94a84b8a9dd99045d80005a40
Vũ Bá Quang <vubaquang1998@gmail.com>
2020-09-05 11:12:25 +0700

    .gitignore

--- vue.config.js
+++ vue.config.js
@@ -31,7 +31,7 @@ module.exports = {
   productionSourceMap: false,
   devServer: {
     port: port,
-    host: 'localhost',
+    // host: 'localhost',
     // https: true,
     https: {
       key: 'localhost-key.pem',
